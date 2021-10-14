f08ed149fbd6850eae79042a148b13c795f8905f
TaniQL <taniaquispelazo@gmail.com>
2021-10-13 19:47:14 -0500

    Route tables

--- BurgerQueen/src/app/app-routing.module.ts
+++ BurgerQueen/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { RolesComponent } from './components/roles/roles.component';
+import { TablesComponent } from './components/tables/tables.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'roles',
     component: RolesComponent,
+  },
+  {
+    path: 'tables',
+    component: TablesComponent,
   }
 ];
 
