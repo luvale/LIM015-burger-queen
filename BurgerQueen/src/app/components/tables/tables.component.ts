cd78c9bf0088e507c06e91c31e287cae65156a72
luvale <valeriae200@hotmail.com>
2021-10-18 11:01:47 -0500

    Imprimir las mesas y asignarles id

--- BurgerQueen/src/app/components/tables/tables.component.ts
+++ BurgerQueen/src/app/components/tables/tables.component.ts
@@ -9,43 +9,25 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 })
 export class TablesComponent implements OnInit {
   tables: any[] = [];
-  //public tables: any[] = [];
-  //items: Observable<any[]>;
   
   constructor(
     private service: FirestoreService
-    ) {   /*this.service.mesas().subscribe(
-      table => { console.log(table) }
-    ); */
-  }
-  // constructor() {}
+    ) {}
 
   ngOnInit(): void {
     this.mesas();
-    /*
-    this.service.mesas().subscribe(
-      table => { console.log(table) }
-    );*/
-  }
-  // this.items = this.service.collection('items').valueChanges();
-   alert() {
-    alert('soyUnaAlerta😎');
   }
 
   mesas() {
-   console.log(this.service.mesas().subscribe((data) => console.log(data)));
-  } 
-  /*
-  mesas() {
-    this.service.mesas().subscribe((data) => {
-          this.tables = [];
-          data.forEach((mesaData:any) => {
-            this.tables.push({
-            id: mesaData.payload.doc.id,
-            data: mesaData.payload.doc.data()
-        })
-      });
-      console.log(this.tables);
-    });
-  } */
+   this.service.mesas().subscribe((data) => {
+     // console.log(data);
+     data.forEach((mesaData) => {
+       this.tables.push({
+          id: mesaData.payload.doc.id,
+         data: mesaData.payload.doc.data()
+      })
+     });
+     console.log(this.tables);
+    })
+  }
 }
