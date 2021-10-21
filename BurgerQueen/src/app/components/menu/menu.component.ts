6109cd2ae8141bc4b5813ae3d1397b61e51bc275
luvale <valeriae200@hotmail.com>
2021-10-21 14:41:53 -0500

    creo cardClick() que muestra en consola los items del menu que han sido clickeados

--- BurgerQueen/src/app/components/menu/menu.component.ts
+++ BurgerQueen/src/app/components/menu/menu.component.ts
@@ -8,8 +8,9 @@ import { FirestoreService } from 'src/app/services/firestore.service';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  productos:any [] = [];
+  allProducts:any [] = [];
   menu: any[] = [];
+  cart: any[] = [];
 
   constructor(private service: FirestoreService) { }
 
@@ -19,13 +20,17 @@ export class MenuComponent implements OnInit {
   getMenu(){
     this.service.getMenu().subscribe((data) => {
       data.forEach((item) => {
-        this.productos.push({
+        this.allProducts.push({
           id: item.payload.doc.id,
           data: item.payload.doc.data()})
       })
     })
    }
    filteredMenu($event:any){
-    this.menu = this.productos.filter((item) => item.data.horario == $event.target.value);
+    this.menu = this.allProducts.filter((item) => item.data.horario == $event.target.value);
+   }
+   cardClick(data:Object) {
+    this.cart.push({data});
+    console.log(this.cart)
    }
 }
