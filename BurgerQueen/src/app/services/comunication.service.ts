61d22adaf5219f5065c47ffa947ce4996e805b55
luvale <valeriae200@hotmail.com>
2021-11-02 20:24:40 -0500

    Corrijo servicio comunication

--- BurgerQueen/src/app/services/comunication.service.ts
+++ BurgerQueen/src/app/services/comunication.service.ts
@@ -1,9 +1,16 @@
-import { EventEmitter, Injectable, Output } from '@angular/core';
+import { EventEmitter, Injectable, Output} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComunicationService {
-  // constructor() { }
-  @Output() disparador: EventEmitter<any> = new EventEmitter();
+  // constructor() {}
+ // @Output() disparador: EventEmitter<any> = new EventEmitter();
+ disparador = new BehaviorSubject({});
+ disparadorObservable = this.disparador.asObservable();
+
+ sendSelectedTable(table:Object) {
+  this.disparador.next(table);
+ }
 }
